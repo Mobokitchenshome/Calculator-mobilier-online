b84cef6ab0ae67dbd12f2c6e7998a474550762b0
Mobokitchenshome <stoianiurii@gmail.com>
2025-03-04 00:31:52 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,25 @@
+// Importă Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-analytics.js";
+
 // Configurare Firebase
 const firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "proiectul-tau.firebaseapp.com",
-    databaseURL: "https://proiectul-tau.firebaseio.com",
-    projectId: "proiectul-tau",
-    storageBucket: "proiectul-tau.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "1:123456789:web:abcdefghij"
+    apiKey: "AIzaSyDVAp7PaK7dNksfVhjXio4svEraal-7x_M",
+    authDomain: "mobokitchenshome.firebaseapp.com",
+    databaseURL: "https://mobokitchenshome-default-rtdb.firebaseio.com",
+    projectId: "mobokitchenshome",
+    storageBucket: "mobokitchenshome.appspot.com", // ✅ Corectat
+    messagingSenderId: "1060936386110",
+    appId: "1:1060936386110:web:cc26a6a3741b4d1bdff36f",
+    measurementId: "G-E9CPCNXR7V"
 };
-firebase.initializeApp(firebaseConfig);
-const storage = firebase.storage();
-const database = firebase.database();
+
+// Inițializează Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const storage = getStorage(app);
+const analytics = getAnalytics(app);
+
+export { database, storage };
